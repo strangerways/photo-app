2c30129af8db22040cd3766cce18c65a2e1a3498
John <john.parkinson@covenanteyes.com>
2017-09-05 14:12:22 +0000

    email functionality

--- config/environment.rb
+++ config/environment.rb
@@ -9,8 +9,8 @@ ActionMailer::Base.smtp_settings ={
   :port => '587',
   :authentication => :plain,
   :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENB['SENDGRID_PASSWORD'],
-  :domain => 'heroku.com0',
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'heroku.com',
   :enable_starttls_auto => true,
   
   
